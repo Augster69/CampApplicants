3acb195c33cbecf4e401d96d1ca7e0ded15e8fc0
Augster69 <august.cornfeld@students.thelangschool.org>
2023-10-04 11:28:30 -0400

    modified:   .vscode/launch.json 	modified:   ApplicantProcessor.java 	modified:   Ui.java

--- ApplicantProcessor.java
+++ ApplicantProcessor.java
@@ -9,7 +9,15 @@ public class ApplicantProcessor {
      * for all other ages return null;
      */
     public String processApplicant(Applicant applicant){
-        // part 1: put in conditionals that will return the correct string as shown above
-        return null;
+       if(applicant.getAge() >=4 && applicant.getAge() <= 8){ 
+        return "Sparrow";
+       }
+        if(applicant.getAge() > 8 && applicant.getAge() <= 13){
+            return "Robin";
+        }
+        if(applicant.getAge() >13 && applicant.getAge() <=18){
+            return "Eagle";
+        }
+        return "Not applicable for camp. Sorry! :(";
     }
 }
